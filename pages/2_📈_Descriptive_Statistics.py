7b581c306b37b2a969fe9cff9261e3b78f8c359a
Nicolás Mella <nicolasmella2004@gmail.com>
2025-02-06 19:57:09 -0300

    Descriptive statistics added

--- pages/2_📈_Descriptive_Statistics.py
+++ pages/2_📈_Descriptive_Statistics.py
@@ -3,11 +3,24 @@ from DataAnalysis import DataAnalysis
 
 st.set_page_config(page_title="Descriptive Statistics", page_icon="📈")
 
-st.markdown("# Descriptive Statistics")
+st.markdown("""
+            # Descriptive Statistics
+            Summary statistics of the Dataframe provided""")
 
 if "data" in st.session_state:
     df = st.session_state["data"]
 
-    st.write(df.describe())
+    st.write("## Summary Table")
+    column_names = list(df.columns)
+    option = st.selectbox("Show Table", column_names,index=None, placeholder="Select a column name...")
+    if option:
+        st.write(df[option].describe())
+    else:
+        st.write(df.describe())
+    
+    st.write("## Central Tendency")
+    st.write(df.mean(numeric_only=True).to_frame(name="Mean"))
+    st.write(df.median(numeric_only=True).to_frame(name="Median"))
+    st.write(df.mode().iloc[0].to_frame(name="Mode"))
 else:
     st.error("No data uploaded")
