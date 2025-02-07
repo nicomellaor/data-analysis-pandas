dbc5b67abc8209315bef491b1fa86687043f2670
Nicolás Mella <nicolasmella2004@gmail.com>
2025-02-06 21:03:35 -0300

    Histogram and boxplot added

--- pages/3_📊_Histogram_&_Boxplot.py
+++ pages/3_📊_Histogram_&_Boxplot.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from DataAnalysis import DataAnalysis
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.set_page_config(page_title="Histogram & Boxplot", page_icon="📊")
 
@@ -7,6 +9,26 @@ st.markdown("# Histogram & Boxplot")
 
 if "data" in st.session_state:
     df = st.session_state["data"]
+    new_df = df.select_dtypes(include=np.number)
+    
+    column_names = list(new_df.columns)
+    option = st.selectbox("Show graphics", column_names,index=None, placeholder="Select a column name...")
+    if option:
+        st.write("## Histogram")
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.hist(df[option], bins=10, rwidth=0.95, color="lightsalmon")
+        ax.set_xlabel(f"{option}")
+        ax.set_ylabel("Frequency")
+        ax.yaxis.grid(color="lightgray", linestyle="--")
+        st.pyplot(fig)
+    
+        st.write("## Boxplot")
+        fig, ax = plt.subplots(figsize=(8, 4))
+        sns.boxplot(x=df[option], color="lightsalmon", ax=ax)
+        ax.set_title(f"Boxplot {option}")
+        ax.xaxis.grid(color="lightgray", linestyle="--")
+        st.pyplot(fig)
+
 
 else:
     st.error("No data uploaded")
